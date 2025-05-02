78cd8b2745cf7f3067a6e3bc94340b0e845a4050
teju1403 <tejahoster@gmail.com>
2025-05-02 23:09:35 +0530

    Add files via upload

---
+++ Onlinesales.sql
@@ -0,0 +1,40 @@
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10001, TO_DATE('2023-05-20', 'YYYY-MM-DD'), 'Clothing', 'Zara Jacket', 5, 901.73, 4508.65, 'South America', 'PayPal');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10002, TO_DATE('2023-02-19', 'YYYY-MM-DD'), 'Home Appliances', 'Philips Air Fryer', 4, 1203.95, 4815.80, 'North America', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10003, TO_DATE('2024-08-05', 'YYYY-MM-DD'), 'Home Appliances', 'LG Washing Machine', 5, 1017.48, 5087.40, 'North America', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10004, TO_DATE('2023-07-29', 'YYYY-MM-DD'), 'Electronics', 'Sony Headphones', 4, 852.86, 3411.44, 'Asia', 'Credit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10005, TO_DATE('2023-07-23', 'YYYY-MM-DD'), 'Electronics', 'MacBook Air', 2, 776.45, 1552.90, 'South America', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10006, TO_DATE('2024-09-06', 'YYYY-MM-DD'), 'Beauty Products', 'Neutrogena Skincare Set', 3, 491.15, 1473.45, 'Europe', 'Credit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10007, TO_DATE('2024-06-30', 'YYYY-MM-DD'), 'Beauty Products', 'Oreal Shampoo', 3, 882.91, 2648.73, 'North America', 'PayPal');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10008, TO_DATE('2023-05-09', 'YYYY-MM-DD'), 'Clothing', 'LeviJeans', 5, 669.54, 3347.70, 'Europe', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10009, TO_DATE('2023-12-30', 'YYYY-MM-DD'), 'Beauty Products', 'Dove Soap Pack', 1, 954.44, 954.44, 'Europe', 'Cash on Delivery');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10010, TO_DATE('2023-03-07', 'YYYY-MM-DD'), 'Books', 'The Alchemist', 1, 969.59, 969.59, 'Europe', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10011, TO_DATE('2024-03-15', 'YYYY-MM-DD'), 'Beauty Products', 'Neutrogena Skincare Set', 1, 898.60, 898.60, 'Asia', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10012, TO_DATE('2023-06-11', 'YYYY-MM-DD'), 'Electronics', 'MacBook Air', 1, 557.17, 557.17, 'Asia', 'PayPal');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10013, TO_DATE('2025-01-02', 'YYYY-MM-DD'), 'Beauty Products', 'Dove Soap Pack', 1, 1484.20, 1484.20, 'North America', 'Credit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10014, TO_DATE('2024-07-11', 'YYYY-MM-DD'), 'Clothing', 'Nike Running Shoes', 1, 1157.55, 1157.55, 'North America', 'PayPal');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10015, TO_DATE('2024-11-10', 'YYYY-MM-DD'), 'Beauty Products', 'Dove Soap Pack', 5, 393.72, 1968.60, 'North America', 'Credit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10016, TO_DATE('2024-07-30', 'YYYY-MM-DD'), 'Books', 'Atomic Habits', 4, 910.57, 3642.28, 'Asia', 'Cash on Delivery');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10017, TO_DATE('2025-03-28', 'YYYY-MM-DD'), 'Home Appliances', 'Instant Pot Duo', 5, 1221.34, 6106.70, 'North America', 'Credit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10018, TO_DATE('2023-04-11', 'YYYY-MM-DD'), 'Beauty Products', 'Maybelline Lipstick', 1, 1429.84, 1429.84, 'Europe', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10019, TO_DATE('2023-01-30', 'YYYY-MM-DD'), 'Books', 'Atomic Habits', 5, 1308.79, 6543.95, 'North America', 'Debit Card');
+INSERT INTO Transactions (Transaction_ID, order_date, Product_Category, Product_Name, Units_Sold, Unit_Price, Total_Revenue, Region, Payment_Method)
+VALUES (10020, TO_DATE('2023-07-20', 'YYYY-MM-DD'), 'Home Appliances', 'LG Washing Machine', 5, 596.96, 2984.80, 'Europe', 'PayPal');
